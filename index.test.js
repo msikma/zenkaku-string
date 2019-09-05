403c6cdf89e6042a74eeee4d9f1803dac11b6873
Michiel Sikma <michiel@sikma.org>
2019-09-06 01:14:36 +0200

    Include the rest of the fullwidth test cases

--- index.test.js
+++ index.test.js
@@ -26,9 +26,8 @@ describe('CJK Length', () => {
       expect(cjkLength('ｵﾊﾖｳｺﾞｻﾞｲﾏｽ')).toBe(cjkLength('00110011001')) // note: extra space for the dakuten
       expect(cjkLength('ｵﾊﾖｳｺﾞｻﾞｲﾏｽ')).not.toBe(cjkLength('オハヨウゴザイマス'))
     })
-    it('contains ideographic symbols and punctuation', () => {
-      expect(cjkLength('\u3000')).toBe(2) // ideographic space
-      expect(cjkLength('、。【】')).toBe(8)
+    it('contains CJK unified ideographs', () => {
+      expect(cjkLength('一龥')).toBe(4)
     })
     it('contains ideographic symbols and punctuation', () => {
       expect(cjkLength('\u3000')).toBe(2) // ideographic space
@@ -38,9 +37,24 @@ describe('CJK Length', () => {
       expect(cjkLength('ᄀᄁᄂᄃᄄᄅᄆᄇ')).toBe(16)
       expect(cjkLength('ㄱ..ㆎ')).toBe(6)
     })
+    it('contains Hangul syllables', () => {
+      expect(cjkLength('가각갂갃간갅갆갇갈갉갊갋갌갍갎갏')).toBe(32)
+      expect(cjkLength('뀀뀁뀂뀃뀄뀅뀆뀇뀈뀉뀊뀋뀌뀍뀎뀏')).toBe(32)
+      expect(cjkLength('쀀쀁쀂쀃쀄쀅쀆쀇쀈쀉쀊쀋쀌쀍쀎쀏')).toBe(32)
+      expect(cjkLength('퀀퀁퀂퀃퀄퀅퀆퀇퀈퀉퀊퀋퀌퀍퀎퀏')).toBe(32)
+    })
     it('contains kanbun characters', () => {
       expect(cjkLength('㆐..㆟')).toBe(6)
       expect(cjkLength('㆐㆑㆒㆓㆔㆕㆖㆗㆘㆙㆚㆛㆜㆝㆞㆟')).toBe(32)
+    })
+    it('contains parenthesized and circled characters', () => {
+      expect(cjkLength('㈀..㈜㈠..㉃㉠..㊰')).toBe(18)
+    })
+    it('contains other CJK characters (compatibility ideographs; enclosed letters and months)', () => {
+      expect(cjkLength('㍻㋀㏠豈鶴')).toBe(10)
+    })
+    it('contains other fullwidth symbols and punctuation', () => {
+      expect(cjkLength('︰..﹄﹉..﹒﹔..﹫！..～￠..￦')).toBe(30)
     })
   })
 })
