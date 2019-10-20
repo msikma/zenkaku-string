17a4c66a4e53519723cf2d6bcb513d4d4566e8f3
Michiel Sikma <michiel@sikma.org>
2019-10-20 18:56:22 +0200

    Finalize first version of wideSlice() and add tests

--- src/sub.js
+++ src/sub.js
@@ -1,6 +1,7 @@
 // © 2019, MIT license
 
 const { charRangeRe, charRangeReGlobal } = require('./characters')
+const { wideLength } = require('./length')
 
 /**
  * Returns a sliced string with wide/fullwidth characters counting for two.
@@ -24,30 +25,34 @@ const { charRangeRe, charRangeReGlobal } = require('./characters')
  * @param   {string} padChar  Character to use for padding the string to the correct length
  * @returns {string}          Sliced string
  */
-const wideSlice = (str, startIdx, endIdx, padChar = '!') => {
-  // TODO: convert startIdx to Number, add tests
+const wideSlice = (str, startIdx = 0, endIdx = Infinity, padChar = ' ') => {
+  // The following cases necessarily result in an empty string as output:
+  // 1) when the end index is zero;
+  if (endIdx === 0) return ''
+  // 2) or when both numbers have the same sign and the end is smaller.
+  if ((startIdx > 0) === (endIdx > 0) && startIdx > endIdx) return ''
 
-  // Special case: return an empty string if start/end is the same,
-  // or if end is smaller than start. String.prototype.slice() behaves the same.
-  if (startIdx >= endIdx) return ''
+  // If there are no wide characters in the input string at all,
+  // the result must necessarily be identical to String.prototype.slice().
+  if (!charRangeRe.test(str)) return str.slice(startIdx, endIdx)
 
-  // Check if there are any wide characters in the input string.
-  // If not, we can just use the regular slice methods.
-  if (!charRangeRe.test(str)) {
-    return str.slice(startIdx, endIdx)
-  }
+  // Negative offsets must be normalized to positive values (so the algorithm can remain simple).
+  // A slice range of e.g. (2, -2) is equal to (2, len - 2). Get the wide length to calculate this.
+  const inLength = wideLength(str)
+  const inStartIdx = Math.max(startIdx < 0 ? inLength + startIdx : startIdx, 0)
+  const inEndIdx = Math.max(endIdx < 0 ? inLength + endIdx : endIdx, 0)
 
-  // Note: the charRangeReGlobal object is stateful. Reset it before use.
-  charRangeReGlobal.lastIndex = 0
+  // One last special case after normalization:
+  if (inStartIdx >= inEndIdx) return ''
 
-  console.log('wideSlice start', { str, startIdx, endIdx });
-  console.log('=')
-
-  const expect2 = str.replace(charRangeReGlobal, 'XX')
-  const expect = expect2.slice(startIdx, endIdx)
-
-  charRangeReGlobal.lastIndex = 0
-
+  // Now we'll iterate over the input using our regex that matches only wide characters.
+  // Each wide character that is passed results in an offset to our slice range.
+  //
+  // For example: for wideSlice('あaえe', 0, 3), we expect 'あa' but need to offset
+  // the end index by 1 to account for that one あ we passed.
+  //
+  // Both the start and end indices are offset by exactly the number of wide characters
+  // we pass on the way to reaching them.
 
   let wideIdx = 0           // Current matched position index plus wide character count ("real" position).
   let wideCount = 0         // Number of wide characters found so far.
@@ -55,84 +60,86 @@ const wideSlice = (str, startIdx, endIdx, padChar = '!') => {
   let realStartIdx = null   // "Real" start index, keeping wide characters into account.
   let realEndIdx = null     // "Real" end index.
 
-  let foundStart = false    // Whether the wide start index has been found yet.
-  let foundEnd = false      // Whether the wide end index has been found yet.
-
-  let hasStartPadding = false // Whether a padding character is needed at the start.
-  let hasEndPadding = false   // Whether a padding character is needed at the end.
+  let foundStart = false    // Whether the wide start/end indices have been found yet.
+  let foundEnd = false
+  let padStart = false      // Whether a padding character is needed at the start/end of the result string.
+  let padEnd = false
   
   // Check if we can set the start index right away.
-  realStartIdx = startIdx === 0 ? 0 : null
-  foundStart = startIdx === 0 ? true : false
+  realStartIdx = inStartIdx === 0 ? 0 : null
+  foundStart = inStartIdx === 0 ? true : false
   
+  // Note: global RegExp objects are stateful. Reset ours before use.
+  charRangeReGlobal.lastIndex = 0
 
   while (true) {
     // Match up to the next wide character.
     match = charRangeReGlobal.exec(str)
 
-    
-
     // If there's no match, we've passed the last wide character in the string.
+    // Note: this will never happen on the first iteration because in that case the string did not
+    // contain any wide characters to begin with--and if that is so, we would have exited before.
     if (!match) {
-      console.log('no match end');
+      if (!foundStart) {
+        realStartIdx = inStartIdx - wideCount
+        foundStart = true
+      }
       if (!foundEnd) {
-        realEndIdx = endIdx - wideCount
+        realEndIdx = inEndIdx - wideCount
         foundEnd = true
       }
       break
     }
 
-
+    // Current index inside the string, if wide characters were counted as two.
     wideIdx = match.index + wideCount
 
-    console.log('match', {'char': match[0], wideIdx, 'exp': expect2.slice(0, wideIdx + 2) })
-
     if (!foundStart) {
-      if (wideIdx >= startIdx) {
-        console.log('sta = 1');
-        realStartIdx = startIdx - wideCount
+      if (wideIdx >= inStartIdx) {
+        realStartIdx = inStartIdx - wideCount
         foundStart = true
       }
-      if (wideIdx + 1 === startIdx) {
-        realStartIdx = startIdx - (wideCount) // for the padding!
+      if (wideIdx + 1 === inStartIdx) {
+        // If one off from the target, the start position is in the middle of a wide character and needs padding.
+        //
+        // Note: in actuality, the real start index is properly calculated as: inStartIdx - 1 - (wideCount - 1),
+        // as we need to make room for a single padding character, and we need to subtract the one
+        // "half" wide character that we cannot display. The following line is identical to that.
+        realStartIdx = inStartIdx - wideCount
         foundStart = true
-        hasStartPadding = true
+        padStart = true
       }
     }
 
     if (!foundEnd) {
-      if (wideIdx >= endIdx) {
-        console.log('end = 1');
-        realEndIdx = endIdx - wideCount
+      if (wideIdx >= inEndIdx) {
+        realEndIdx = inEndIdx - wideCount
         foundEnd = true
       }
-      if (wideIdx + 1 === endIdx) {
-        realEndIdx = endIdx - (wideCount + 1) // for the padding!
+      if (wideIdx + 1 === inEndIdx) {
+        // If one off from the target, the end position is in the middle of a wide character and needs padding.
+        // Subtract the last "half" wide character so we have room for one padding character.
+        realEndIdx = inEndIdx - (wideCount + 1)
         foundEnd = true
-        hasEndPadding = true
+        padEnd = true
       }
     }
 
-
+    // Check if we're done yet.
     if (foundStart && foundEnd) {
       break
     }
 
+    // Passed one wide character.
     wideCount += 1
   }
-
-  console.log('=')
-
-  console.log('wideSlice end', { wideCount, startIdx, endIdx, realStartIdx, realEndIdx })
-
   
-  
-  console.log(expect, 'done');
+  // Reset our RegExp object again as a courtesy to the next user.
+  charRangeReGlobal.lastIndex = 0
 
-  if (hasStartPadding) {
-    return padChar + str.slice(realStartIdx, realEndIdx) + (hasEndPadding ? padChar : '')
-  }
-  return str.slice(realStartIdx, realEndIdx) + (hasEndPadding ? padChar : '')
+  // Now that we have the correct range, offset appropriately to take wide characters into account,
+  // we can slice the input by that range and add padding as needed to ensure the string's length is as expected.
+  return (padStart ? padChar : '') + str.slice(realStartIdx, realEndIdx) + (padEnd ? padChar : '')
 }
 
 const wideSubstr = () => {
--- src/sub.test.js
+++ src/sub.test.js
@@ -4,17 +4,99 @@ const { wideSlice } = require('./sub')
 
 describe('Zenkaku-string', () => {
   describe('wideSlice()', () => {
-    describe('string slicing correctly takes wide characters into account when', () => {
-      it('contains only non-wide characters', () => {
+    describe('string slicing behaves exactly as String.prototype.slice() when', () => {
+      it('is given incorrect or nonsensical index values', () => {
         expect(wideSlice('asdf', 2)).toBe('asdf'.slice(2))
+        expect(wideSlice('asdf', 2, 3)).toBe('asdf'.slice(2, 3))
+        expect(wideSlice('asdf', 1234, -1234)).toBe('asdf'.slice(1234, -1234))
+        expect(wideSlice('asdf', Infinity, -Infinity)).toBe('asdf'.slice(Infinity, -Infinity))
+        expect(wideSlice('asdf', 'z')).toBe('asdf'.slice('z'))
+        expect(wideSlice('asdf', null)).toBe('asdf'.slice(null))
+        expect(wideSlice('asdf', null, null)).toBe('asdf'.slice(null, null))
+        expect(wideSlice('asdf', Number, Array)).toBe('asdf'.slice(Number, Array))
+        expect(wideSlice('asdf', () => console.log('hi'), Array)).toBe('asdf'.slice(() => console.log('hi'), Array))
+        expect(wideSlice('asdf', 3, 2)).toBe('asdf'.slice(3, 2))
+        expect(wideSlice('asdf', -2)).toBe('asdf'.slice(-2))
+        expect(wideSlice('asdf', -2, -1)).toBe('asdf'.slice(-2, -1))
+      })
+    })
+
+    describe('string slicing correctly takes wide characters into account when', () => {
+      it('contains only regular characters', () => {
+        expect(wideSlice('asdf', -2)).toBe('df')
+        expect(wideSlice('asdf', -1)).toBe('f')
+        expect(wideSlice('asdf', 2)).toBe('df')
+        expect(wideSlice('asdf', 1)).toBe('sdf')
+        expect(wideSlice('asdf', 1, -1)).toBe('sd')
+        expect(wideSlice('asdf', 1, 0)).toBe('')
+        expect(wideSlice('asdf', 1, 1)).toBe('')
+        expect(wideSlice('asdf', 0, 2)).toBe('as')
+        expect(wideSlice('asdf', 1, 2)).toBe('s')
+        expect(wideSlice('asdf', 1, 3)).toBe('sd')
+        expect(wideSlice('asdf', 2, 3)).toBe('d')
+        expect(wideSlice('asdf', 500)).toBe('')
+        expect(wideSlice('Lorem ipsum dolor sit amet, consectetur adipiscing elit.', 0, 5)).toBe('Lorem')
+        expect(wideSlice('Aenean pulvinar porttitor ex, in lobortis quam vehicula vel. Maecenas est sem, faucibus et pulvinar in, euismod quis leo.', 61)).toBe('Maecenas est sem, faucibus et pulvinar in, euismod quis leo.')
+      })
+
+      it('contains only wide characters', () => {
+        expect(wideSlice('あ', 2)).toBe('')
+        expect(wideSlice('あ', 0)).toBe('あ')
+        expect(wideSlice('あ', 0, 2)).toBe('あ')
+        expect(wideSlice('あい', 0, 2)).toBe('あ')
+        expect(wideSlice('あいえよ', 0, 2)).toBe('あ')
+        expect(wideSlice('あいえよ', 2, 2)).toBe('')
+        expect(wideSlice('あいえよ', 2, 4)).toBe('い')
+        expect(wideSlice('あいえよ', 2, 6)).toBe('いえ')
+        expect(wideSlice('あいえよ', 2, -2)).toBe('いえ')
+        expect(wideSlice('あいえよ', -2, -2)).toBe('')
+        expect(wideSlice('あいえよ', -2)).toBe('よ')
+        expect(wideSlice('あいえよ', -4, 7)).toBe('え ')
+      })
+
+      it('contains a mixture of wide and regular characters', () => {
+        expect(wideSlice('あ', 2)).toBe('')
+        expect(wideSlice('あ', 0)).toBe('あ')
+        expect(wideSlice('あ', 0, 2)).toBe('あ')
+        expect(wideSlice('あaa', 0, 2)).toBe('あ')
+        expect(wideSlice('あaaえaa', 0, 2)).toBe('あ')
+        expect(wideSlice('あaaえaa', 2, 2)).toBe('')
+        expect(wideSlice('あaaえaa', 2, 4)).toBe('aa')
+        expect(wideSlice('あaaえaa', 2, 6)).toBe('aaえ')
+        expect(wideSlice('aaあいえよ', -4, 8)).toBe('え')
       })
     })
 
     describe('string slicing correctly pads result strings when', () => {
-      it('contains wide partially inside the slice range', () => {
-        expect(wideSlice('asdfあい asdfai あいasdf', 2, 8)).toBe('dfあい')
-        expect(wideSlice('asdfあい asdfai あいasdf', 2, 12)).toBe('dfあい asd')
-        expect(wideSlice('asdfあい asdfai あいasdf', 1, 3)).toBe('sdf')
+      it('contains half a wide character at the start', () => {
+        expect(wideSlice('あaaえaa', 1, 2)).toBe(' ')
+        expect(wideSlice('あaaえaa', 1, 2, '!')).toBe('!')
+        expect(wideSlice('あaaえaa', 1, 3, '!')).toBe('!a')
+        expect(wideSlice('あaaえaa', 1, 4, '!')).toBe('!aa')
+        expect(wideSlice('あaaえaa', 1, 6, '!')).toBe('!aaえ')
+        expect(wideSlice('あaaえaa', 1, 7)).toBe(' aaえa')
+        expect(wideSlice('あaaえaa', 5, 7)).toBe(' a')
+        expect(wideSlice('あaaえaa', 5, 8)).toBe(' aa')
+        expect(wideSlice('あaaえaaいう', 5, 8)).toBe(' aa')
+        expect(wideSlice('あaaえaaいうiu', 5, 8)).toBe(' aa')
+        expect(wideSlice('あaaえaaいうiu', 9, 10)).toBe(' ')
+        expect(wideSlice('あaaえaaいうiu', 9, 12)).toBe(' う')
+        expect(wideSlice('あaaえaaいうiu', 9, 13)).toBe(' うi')
+        expect(wideSlice('あaaえaaいうiu', 11, 13)).toBe(' i')
+        expect(wideSlice('あaaえaaいうiu', 11, 14)).toBe(' iu')
+      })
+
+      it('contains half a wide character at the end', () => {
+        expect(wideSlice('あaaえaa', 2, 5)).toBe('aa ')
+        expect(wideSlice('あaaえaa', 2, 5, '!')).toBe('aa!')
+        expect(wideSlice('あaaえeeいiiうuu', 4, 5, '!')).toBe('!')
+        expect(wideSlice('あaaえeeいiiうuu', 4, 5)).toBe(' ')
+        expect(wideSlice('あaaえeeいiiうuu', 4, 9)).toBe('えee ')
+        expect(wideSlice('あaaえeeいiiうuu', 4, 13)).toBe('えeeいii ')
+      })
+
+      it('contains half a wide character on both sides', () => {
+        expect(wideSlice('あaaえeeいiiうuu', 5, 13)).toBe(' eeいii ')
       })
     })
   })
